f62b562e1261ea4485da5569739a4bc7e02e8a63
Thomas Kempf <tkempf@hueper.de>
2019-07-22 12:04:24 +0200

    Already deleted items should not be in the affectedRows when SoftCascading deletes, as this could break Unique Indexes on related models

--- src/SoftCascade.php
+++ src/SoftCascade.php
@@ -199,7 +199,12 @@ class SoftCascade implements SoftCascadeable
         $relationModel = $relation->getQuery()->getModel();
         $relationModel = new $relationModel();
         if ($affectedRows > 0) {
-            $relationModel = $relationModel->withTrashed()->whereIn($foreignKey, $foreignKeyIds)->limit($affectedRows);
+        	if ($this->direction == 'delete') {
+        		$relationModel = $relationModel->whereIn($foreignKey, $foreignKeyIds)->limit($affectedRows);
+        	}
+        	else {
+        		$relationModel = $relationModel->withTrashed()->whereIn($foreignKey, $foreignKeyIds)->limit($affectedRows);
+        	}
             $this->run($relationModel->get([$relationModel->getModel()->getKeyName()]));
             $relationModel->{$this->direction}($this->directionData);
         }
@@ -252,8 +257,12 @@ class SoftCascade implements SoftCascadeable
     {
         $relationModel = $relation->getQuery()->getModel();
         $relationModel = new $relationModel();
-
-        return $relationModel->withTrashed()->whereIn($foreignKey, $foreignKeyIds)->count();
+        if ($this->direction == 'delete') {
+        	return $relationModel->whereIn($foreignKey, $foreignKeyIds)->count();
+        }
+        else {
+        	return $relationModel->withTrashed()->whereIn($foreignKey, $foreignKeyIds)->count();
+        }
     }
 
     /**
